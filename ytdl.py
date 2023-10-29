00f202ca9e7bff987e2f460a89d1ac77a31a6c92
shosdoo <108087046+shosdoo@users.noreply.github.com>
2023-10-29 02:38:12 +0000

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
 from colorama import init, Fore
-from tqdm import tqdm
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
 init()
@@ -13,8 +12,7 @@ YELLOW = "\033[93m"
 RESET = "\033[0m"
 ROSA = "\033[91m\033[97m"
 
-start_time = 0
-end_time = 125
+
 
 url_video = input(f"{Fore.GREEN}Ingresa la url del video:{Fore.RESET} ")
 if url_video.startswith("https://youtu.be"):
@@ -22,23 +20,31 @@ if url_video.startswith("https://youtu.be"):
     if mp3_video == "N":    
         def_name = "video.webm"
         name_sinformat = input(f"{Fore.BLUE}Ingresa un nombre para el video:{Fore.RESET} ")
+        video_recor = input(f"{Fore.BLUE}Quieres recortar el video?(si/no):{Fore.RESET} ")
+        
+        start_time = 0
+        end_time = 125
         name_format = f"{name_sinformat}.mp4"
 
         download_video = f"yt-dlp -o {def_name} {url_video}"
-        result = subprocess.run(download_video, shell=True)#subprocess es para correr el comando shell true para que lo ejecute en una subterminal
+        result = subprocess.run(download_video, shell=True, stdout=subprocess.PIPE)
+        salida = result.stdout
 
         convert = f"ffmpeg -i {def_name} {name_format}"
-        rere = subprocess.run(convert, shell=True)
-        os.remove(def_name)#aqui eliminamos el archivo creado por defecto para dejar solo el que tiene el formato correcto        
-        name_video_noformat = f"2{name_format}"
+        rere = subprocess.run(convert, shell=True, stdout=subprocess.PIPE)
+        sli = rere.stdout
+        os.remove(def_name)        
+        if video_recor == "si":
+            
+            name_video_noformat = f"2{name_format}"        
         
-        
-        ffmpeg_extract_subclip(name_format, start_time, end_time, targetname=name_video_noformat)        
-        os.remove(name_format)
-        name_video_recor = name_video_noformat.lstrip('0123456789')
-        os.rename(name_video_noformat, name_video_recor)
-        print(f"{Fore.BLUE}Se descargo el video:{Fore.RESET} {Fore.GREEN}{name_video_recor}{Fore.RESET}")
-    
+            ffmpeg_extract_subclip(name_format, start_time, end_time, targetname=name_video_noformat)        
+            os.remove(name_format)
+            name_video_recor = name_video_noformat.lstrip('0123456789')
+            os.rename(name_video_noformat, name_video_recor)
+            print(f"{Fore.BLUE}Se descargo el video:{Fore.RESET} {Fore.GREEN}{name_video_recor}{Fore.RESET}")
+        elif video_recor == "no":
+            print(f"{Fore.BLUE}Se descargo el video:{Fore.RESET} {Fore.GREEN}{name_format}{Fore.RESET}")    
     
     elif mp3_video == "Y":
         unamemp3 = "music.webm"
